5a7efaebe44c5ebea36eb8e5e97b7c1e3fc56bc5
Davide Gilardoni <davide.gilardoni@bso.co>
2022-04-05 16:19:50 +0100

    Feature: RSVP search path feature now working. Added bootstrap

--- rsvp/views.py
+++ rsvp/views.py
@@ -21,18 +21,14 @@ class SearchPathPageView(TemplateView):
 
 
 def get_path(request):
+
     search_string = request.POST.get('rsvp_path_name')
     rsvp = RsvpClient(IXR_APY_KEY, IXR_BASE_URL, verify=False)
     result = rsvp.get_rsvp_path_info(search_string)
+
     context = {
         'result': result,
         'search_string': search_string
     }
-    pprint(context)
-    return render(request, 'rsvp/results.html', context)
-#
-# def search_path(request):
-#     form = TextForm(request.GET)
-#     print(form.__dict__)
-#     context = {'form': form}
-#     return render(request, 'rsvp/results.html', context)
+
+    return render(request, 'rsvp/search_path.html', context)
